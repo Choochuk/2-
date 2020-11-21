b7c2fe53a85289c7e4a63691d383f07c9c52b882
Choochuk <74691596+Choochuk@users.noreply.github.com>
2020-11-21 12:01:16 +0300

    Add files via upload

---
+++ 2задания3/2-/zadanie 2/2-/задание2/задание2/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace задание2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn1_Click(object sender, RoutedEventArgs e)
+        {
+            int k = 0;
+            string n = box.Text;
+            for(int i = 0; i < n.Length; i++)
+            {
+                if (n[i] == '.' || n[i] == '!' || n[i] == '?')
+                {
+                    k += 1;
+
+                }
+                boxs.Text = "В тексте " + k + " предложения";
+            }
+        }
+    }
+}
